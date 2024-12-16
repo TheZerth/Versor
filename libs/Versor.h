74ea4791fa7a9b0f85ed4a0a81ba98fd1b0bbc0f
TheZerth <drkainaan@gmail.com>
2024-12-15 22:07:20 -0500

    Changed add/sub/mul/div/inr/ext/lco/rco to use template generics.

--- libs/Versor.h
+++ libs/Versor.h
@@ -30,41 +30,31 @@ public:
     //--------------------Addition----------------------
     template <typename T>
     Versor operator+(const T &t) const { return add(t); }
-
     //--------------------Subtraction--------------------
     template <typename T>
     Versor operator-(const T &t) const { return sub(t); }
-
     //--------------------Multiplication-----------------
     template <typename T>
     Versor operator*(const T &t) const { return mul(t); }
-
     //--------------------Division----------------------
     template <typename T>
     Versor operator/(const T &t) const { return div(t); }
-
     //--------------------Exterior Product--------------------
     template <typename T>
     Versor operator^(const T &t) const { return ext(t); }
-
     //--------------------Interior Product--------------------
     template <typename T>
     Versor operator|(const T &t) const { return inr(t); }
-
     //--------------------Left Contraction--------------------
     template <typename T>
     Versor operator<<(const T &t) const { return lco(t); }
-
     //--------------------Right Contraction--------------------
     template <typename T>
     Versor operator>>(const T &t) const { return rco(t); }
-
     //--------------------Negate--------------------
     Versor operator!() const { return negate(); }
-
     //--------------------Reverse--------------------
     Versor operator~() const { return reverse(); }
-
     //--------------------Boolean Operations--------------------
     bool operator==(const Versor & versor) const    { return a == versor.a &&   x == versor.x &&    y == versor.y &&    b == versor.b; }
     bool operator!=(const Versor & versor) const    { return a != versor.a ||   x != versor.x ||    y != versor.y ||    b != versor.b; }
@@ -72,13 +62,11 @@ public:
     bool operator>=(const Versor & versor) const    { return a >= versor.a &&   x >= versor.x &&    y >= versor.y &&    b >= versor.b; }
     bool operator<(const Versor & versor) const     { return a < versor.a &&    x < versor.x &&     y < versor.y &&     b < versor.b; }
     bool operator<=(const Versor & versor) const    { return a <= versor.a &&   x <= versor.x &&    y <= versor.y &&    b <= versor.b; }
-
     //--------------------IO-STREAM-FUNCTIONS--------------------
     // Console Outputd
     friend std::ostream &operator<<(std::ostream &os, const Versor &v)  { os << std::fixed << std::setprecision(3) << std::setfill('0') << "[" << v.toString() << "]"; return os; }
     // Console Input, create a Versor using A X Y Z input.
     friend std::istream &operator>>(std::istream &is, Versor &v)        { is >> v.a >> v.x >> v.y >> v.b; return is; };
-
 //---------------------------------------------------------------
 //---------------------------FUNCTIONS---------------------------
     Versor normalize() const;
@@ -87,64 +75,23 @@ public:
     Versor reverse() const;
     std::string toString() const;
     std::string toLatex() const;
-
 private:
-    Versor add(const Versor &v) const;
-    Versor add(const std::vector<double> &d) const;
-    Versor add(const std::vector<float> &f) const;
-    Versor add(const std::vector<int> &i) const;
-    Versor add(const double &d) const;
-    Versor add(const float &f) const;
-    Versor add(const int &i) const;
-    Versor sub(const Versor &v) const;
-    Versor sub(const std::vector<double> &d) const;
-    Versor sub(const std::vector<float> &f) const;
-    Versor sub(const std::vector<int> &i) const;
-    Versor sub(const double &d) const;
-    Versor sub(const float &f) const;
-    Versor sub(const int &i) const;
-    Versor mul(const Versor &v) const;
-    Versor mul(const std::vector<double> &d) const;
-    Versor mul(const std::vector<float> &f) const;
-    Versor mul(const std::vector<int> &i) const;
-    Versor mul(const double &d) const;
-    Versor mul(const float &f) const;
-    Versor mul(const int &i) const;
-    Versor div(const Versor &v) const;
-    Versor div(const std::vector<double> &d) const;
-    Versor div(const std::vector<float> &f) const;
-    Versor div(const std::vector<int> &i) const;
-    Versor div(const double &d) const;
-    Versor div(const float &f) const;
-    Versor div(const int &i) const;
-    Versor inr(const Versor &v) const;
-    Versor inr(const std::vector<double> &d) const;
-    Versor inr(const std::vector<float> &f) const;
-    Versor inr(const std::vector<int> &i) const;
-    Versor inr(const double &d) const;
-    Versor inr(const float &f) const;
-    Versor inr(const int &i) const;
-    Versor ext(const Versor &v) const;
-    Versor ext(const std::vector<double> &d) const;
-    Versor ext(const std::vector<float> &f) const;
-    Versor ext(const std::vector<int> &i) const;
-    Versor ext(const double &d) const;
-    Versor ext(const float &f) const;
-    Versor ext(const int &i) const;
-    Versor lco(const Versor &v) const;
-    Versor lco(const std::vector<double> &d) const;
-    Versor lco(const std::vector<float> &f) const;
-    Versor lco(const std::vector<int> &i) const;
-    Versor lco(const double &d) const;
-    Versor lco(const float &f) const;
-    Versor lco(const int &i) const;
-    Versor rco(const Versor &v) const;
-    Versor rco(const std::vector<double> &d) const;
-    Versor rco(const std::vector<float> &f) const;
-    Versor rco(const std::vector<int> &i) const;
-    Versor rco(const double &d) const;
-    Versor rco(const float &f) const;
-    Versor rco(const int &i) const;
+    template <typename T>
+    Versor add(const T &t) const;
+    template <typename T>
+    Versor sub(const T &t) const;
+    template <typename T>
+    Versor mul(const T &t) const;
+    template <typename T>
+    Versor div(const T &t) const;
+    template <typename T>
+    Versor inr(const T &t) const;
+    template <typename T>
+    Versor ext(const T &t) const;
+    template <typename T>
+    Versor lco(const T &t) const;
+    template <typename T>
+    Versor rco(const T &t) const;
 };
 
 } // VRSR
